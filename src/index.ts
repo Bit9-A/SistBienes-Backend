6b314e6da968b02ef22556bde86d82348593011b
Bit9-A <adrian24vergel@gmail.com>
2025-04-11 21:21:34 -0400

    Init Backend

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import homeRoute from "./router/home.route";
+
+import { testConnection } from "./database/index";
+
+testConnection();
+
+const __dirname = import.meta.dirname;
+console.log(__dirname);
+
+const app = express();
+
+app.use("/", homeRoute);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port http://localhot:${PORT}`);
+});
