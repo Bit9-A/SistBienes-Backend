d8dffce4b0a01c7cf0d021c5118277bccbb63ce8
Yoangel Godoy <yoangelagodoych201104@gmail.com>
2025-05-25 14:54:38 -0400

    Reorganizar el código de rutas en home.route.ts para mejorar la legibilidad

--- src/modules/home/home.route.ts
+++ src/modules/home/home.route.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import {mueblesController} from "./home.controller";
-
+import { mueblesController } from "./home.controller";
 const router = Router();
 
 // router.get("/", (_, res) => {
 //   res.send("Hello World!");
 // });
-
 router.get("/", mueblesController.getCounts);
-
 export default router;
+
