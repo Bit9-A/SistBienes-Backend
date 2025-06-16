8edfa8ae3f9a3c881e29bcc64c2ab4a39ef84641
Bit9-A <adrian24vergel@gmail.com>
2025-06-15 20:09:25 -0400

    Refactor: Update database schema and missing goods model
    This commit updates the database schema with the latest version, including new tables, columns, and relationships. It also modifies the missing goods model to align with the updated database structure. These changes ensure data consistency and improve application functionality.

--- src/modules/missing-goods/missing-goods.model.ts
+++ src/modules/missing-goods/missing-goods.model.ts
@@ -2,14 +2,34 @@ import { pool } from "../../database/index";
 
 const getAllMissingGoods = async () => {
     const query = `
-        SELECT * FROM BienesFaltantes`;
+    SELECT bf.*, 
+           CONCAT(u.nombre, ' ', u.apellido) AS funcionario_nombre,
+           CONCAT(j.nombre, ' ', j.apellido) AS jefe_nombre,
+           d.nombre AS departamento,
+           m.numero_identificacion AS numero_identificacion
+    FROM BienesFaltantes bf
+    JOIN Usuarios u ON bf.funcionario_id = u.id
+    JOIN Usuarios j ON bf.jefe_id = j.id
+    LEFT JOIN Dept d ON bf.unidad = d.id
+    LEFT JOIN Muebles m ON bf.bien_id = m.id
+  `;
     const [rows] = await pool.query(query);
     return rows as any[];
 };
 
 const getMissingGoodsById = async (id: number) => {
     const query = `
-        SELECT * FROM BienesFaltantes WHERE id = ?`;
+    SELECT bf.*, 
+           CONCAT(u.nombre, ' ', u.apellido) AS funcionario_nombre,
+           CONCAT(j.nombre, ' ', j.apellido) AS jefe_nombre,
+           d.nombre AS departamento,
+           m.numero_identificacion AS numero_identificacion
+    FROM BienesFaltantes bf
+    JOIN Usuarios u ON bf.funcionario_id = u.id
+    JOIN Usuarios j ON bf.jefe_id = j.id
+    LEFT JOIN Dept d ON bf.unidad = d.id
+    LEFT JOIN Muebles m ON bf.bien_id = m.id
+        WHERE bf.id = ?`;
     const [rows] = await pool.execute(query, [id]);
     return (rows as any[])[0];
 };
